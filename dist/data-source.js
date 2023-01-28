85214f11d8ec911234e858a6f97c84e68ab7a76e
Breno Lobianco <99978122+brenolobianco@users.noreply.github.com>
2023-01-27 22:32:04 -0300

    fix files

--- dist/data-source.js
+++ dist/data-source.js
@@ -3,19 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-const path_1 = __importDefault(require("path"));
-require("dotenv/config");
 require("reflect-metadata");
-const AppDataSource = new typeorm_1.DataSource(process.env.NODE_ENV === "test" ?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
-        type: 'postgres',
+require("dotenv/config");
+const path_1 = __importDefault(require("path"));
+const typeorm_1 = require("typeorm");
+const setDataSourceConfig = () => {
+    const entitiesPath = path_1.default.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath = path_1.default.join(__dirname, "./migrations/**.{js,ts}");
+    const nodeEnv = process.env.NODE_ENV;
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
+    }
+    if (nodeEnv === "test") {
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [entitiesPath],
+        };
+    }
+    return {
+        type: "postgres",
         host: process.env.PGHOST,
         username: process.env.PGUSER,
         password: process.env.PGPASSWORD,
@@ -23,8 +36,10 @@ const AppDataSource = new typeorm_1.DataSource(process.env.NODE_ENV === "test" ?
         database: process.env.DB,
         synchronize: false,
         logging: true,
-        entities: [path_1.default.join(__dirname, "./entities/*.{ts,js}")],
-        migrations: [path_1.default.join(__dirname, "./migrations/*.{ts,js}")]
-    });
-exports.default = AppDataSource;
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+};
+const dataSourceConfig = setDataSourceConfig();
+exports.default = new typeorm_1.DataSource(dataSourceConfig);
 //# sourceMappingURL=data-source.js.map
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,27 +1,48 @@
-import { DataSource } from "typeorm"
-import path from "path"
-import "dotenv/config"
 import "reflect-metadata";
+import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
-        type: 'postgres',
-        host: process.env.PGHOST,
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        port: parseInt(process.env.PGPORT),
-        database: process.env.DB,
-        synchronize: false,
-        logging: true,
-        entities: [path.join(__dirname, "./entities/*.{ts,js}")],
-        migrations: [path.join(__dirname, "./migrations/*.{ts,js}")]
-    })
+const setDataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{js,ts}"
+  );
 
-export default AppDataSource
+  const nodeEnv = process.env.NODE_ENV;
+
+  if(nodeEnv === "production"){
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    }
+  }
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.PGHOST,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: parseInt(process.env.PGPORT),
+    database: process.env.DB,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const dataSourceConfig = setDataSourceConfig();
+export default new DataSource(dataSourceConfig);
